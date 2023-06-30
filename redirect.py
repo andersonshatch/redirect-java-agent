249818c9cd3ad41d587072d0b262296a4da55458
Josh Anderson <josh.anderson@contrastsecurity.com>
2023-06-30 12:22:00 +0100

    Output Azure static site redirs, manual overrides

--- redirect.py
+++ redirect.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import requests
 from requests.adapters import HTTPAdapter, Retry
+import json
 import os
 
 session = requests.Session()
@@ -18,8 +19,13 @@ MAVEN_SEARCH_PARAMS = {
     "start": "0",
 }
 
-versions_count = -1
-versions = []
+if versions_override := os.environ.get("VERSIONS_OVERRIDE"):
+    versions = json.loads(versions_override)
+    versions_count = len(versions)
+else:
+    versions = []
+    versions_count = -1
+
 
 while versions_count == -1 or len(versions) < versions_count:
     MAVEN_SEARCH_PARAMS["start"] = str(len(versions))
@@ -31,9 +37,9 @@ while versions_count == -1 or len(versions) < versions_count:
     )
     response_json = response.json()
     versions_count = response_json["response"]["numFound"]
-    versions.extend(response_json["response"]["docs"])
+    versions.extend((version["v"] for version in response_json["response"]["docs"]))
 
-latest_version = versions[0]["v"]
+latest_version = versions[0]
 print(f"got {len(versions)} versions")
 
 
@@ -41,30 +47,37 @@ def maven_download_url(version: str):
     return f"https://search.maven.org/remotecontent?filepath=com/contrastsecurity/contrast-agent/{version}/contrast-agent-{version}.jar"
 
 
+def route_redirect(version: str, dest: str, status_code: int = 302):
+    return {"route": version, "redirect": dest, "statusCode": status_code}
+
+
 latest_version_url = maven_download_url(latest_version)
 
 output_dir = Path("publish")
 output_dir.mkdir(exist_ok=True)
-output_file = output_dir / Path("_redirects")
+output_file = output_dir / Path("staticwebapp.config.json")
+routes = []
 
-lines = []
-lines.append(f"{REDIRECT_PREFIX}latest\t{latest_version_url}")
+routes.append(route_redirect(f"{REDIRECT_PREFIX}latest", latest_version_url))
 print(f"selecting latest as {latest_version}")
 
 
 latest_major_versions = {}
 
 for version_data in versions:
-    version = version_data["v"]
+    version = version_data
     major = version.split(".")[0]
     if major not in latest_major_versions:
-        version = version_data["v"]
+        version = version_data
         latest_major_versions[major] = version
-        lines.append(f"{REDIRECT_PREFIX}{major}\t{maven_download_url(version)}")
+        routes.append(
+            route_redirect(f"{REDIRECT_PREFIX}{major}", maven_download_url(version))
+        )
         print(f"selecting latest of v{major} as {version}")
 
 
-output_file.write_text("\n".join(lines))
+config = {"routes": routes}
+output_file.write_text(json.dumps(config))
 
-print("_redirects file contents:")
+print("file contents:")
 print(output_file.read_text())
